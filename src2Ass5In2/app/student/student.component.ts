225c881c69fb45762b3d4216a4f4f05e3be8d5c4
Abhishek <ar16chimanpure@gmail.com>
2018-07-07 06:44:26 +0530

    first commit

---
+++ src2Ass5In2/app/student/student.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public fun(value)
+  {
+    console.log(value);
+  }
+
+}
